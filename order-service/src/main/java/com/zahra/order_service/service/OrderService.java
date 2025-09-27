b5568136313e48d62466b777e67662d526e02763
unknown <zhr.ghazimirsaeed@gmail.com>
2025-09-27 17:59:26 +0330

    order and product

---
+++ order-service/src/main/java/com/zahra/order_service/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.zahra.order_service.service;
+
+import com.zahra.order_service.dto.OrderLineItemsDto;
+import com.zahra.order_service.dto.OrderRequest;
+import com.zahra.order_service.model.Order;
+import com.zahra.order_service.model.OrderLineItems;
+import com.zahra.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor    //create constructor automatically
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder( OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        return orderLineItems;
+    }
+}
---
+++ product-service/src/main/java/com/zahra/product_service/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.zahra.product_service.service;
+
+import com.zahra.product_service.dto.ProductRequest;
+import com.zahra.product_service.dto.ProductResponse;
+import com.zahra.product_service.model.Product;
+import com.zahra.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j //for logs
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);  //store inside the repository
+        log.info("Product {} is saved", product.getId());   //slf4j dynamically get the id
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
